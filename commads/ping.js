bfe09ea8303b21df3f732bc79ad83c0bb3a2406d
Petr <162743671+petuls@users.noreply.github.com>
2024-10-08 08:37:35 +0200

    updated, added info (for command handler) and measure time.

--- commads/ping.js
+++ commads/ping.js
@@ -1,12 +1,18 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
+    name: 'ping',
     data: new SlashCommandBuilder()
         .setName('ping')
         .setDescription('Replies with Pong! and measures the response time.'),
     async execute(interaction) {
         const sent = await interaction.reply({ content: 'Pong!', fetchReply: true });
         const timeTaken = sent.createdTimestamp - interaction.createdTimestamp;
-        await interaction.editReply(`Pong! This message had a latency of ${timeTaken}ms.`);
+        await interaction.editReply(`Pong! ${timeTaken}ms`);
     },
+    info: {
+        name: 'ping',
+        description: 'Replies with Pong! and measures the response time.',
+        usage: '/ping'
+    }
 };
